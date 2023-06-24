5be9a8859ae924899b13eee432a70103ac463866
SilverKnight13 <jeffrin1395@gmail.com>
2023-06-24 16:06:17 +0530

    hello

--- src/main/java/com/example/makemytrip/train.java
+++ src/main/java/com/example/makemytrip/train.java
@@ -3,9 +3,8 @@ package com.example.makemytrip;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-public class hotel {
-    @GetMapping("/myhotel")
-    public String getData() { return "Please book your hotel tickets from makemytrip";}
+public class train {
+    @GetMapping("/mytrain")
+    public String getData() { return "Please book your hotel tickets from makemytrip for train";}
 }
